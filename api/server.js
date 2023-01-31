90d6e5db4625d1816f1e262b904d919bae05d768
nvtung194208 <tung.nv194208@sis.hust.edu.vn>
2023-01-31 09:44:35 +0700

    remove useCreateIndex

--- api/server.js
+++ api/server.js
@@ -1,40 +1,40 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const multer = require('multer');
-const { responseError, callRes } = require('./response/error');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const multer = require('multer')
+const { responseError, callRes } = require('./response/error')
 
-const app = express();
+const app = express()
 
 // use express.json as middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 // connect to MongoDB
-const url = process.env.mongoURI;
-mongoose.set('strictQuery', true);
+const url = process.env.mongoURI
+mongoose.set('strictQuery', true)
 mongoose
   .connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
   })
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(`errors: ${err}`));
+  .catch((err) => console.log(`errors: ${err}`))
 
 // use Routes
-app.use('/it4788/auth', require('./routes/auth'));
-app.use('/it4788/friend', require('./routes/friend'));
-app.use('/it4788/post', require('./routes/posts'));
-app.use('/it4788/search', require('./routes/search'));
-app.use('/it4788/comment', require('./routes/comments'));
-app.use('/it4788/like', require('./routes/likes'));
-app.use('/it4788/friend', require('./routes/friend'));
-app.use('/it4788/setting', require('./routes/settings'));
-app.use('/it4788/user', require('./routes/user'));
-app.use('/it4788/chat', require('./routes/chat'));
+app.use('/it4788/auth', require('./routes/auth'))
+app.use('/it4788/friend', require('./routes/friend'))
+app.use('/it4788/post', require('./routes/posts'))
+app.use('/it4788/search', require('./routes/search'))
+app.use('/it4788/comment', require('./routes/comments'))
+app.use('/it4788/like', require('./routes/likes'))
+app.use('/it4788/friend', require('./routes/friend'))
+app.use('/it4788/setting', require('./routes/settings'))
+app.use('/it4788/user', require('./routes/user'))
+app.use('/it4788/chat', require('./routes/chat'))
 app.use(function (err, req, res, next) {
   if (err instanceof multer.MulterError) {
     if (err.code === 'LIMIT_UNEXPECTED_FILE') {
@@ -44,13 +44,13 @@ app.use(function (err, req, res, next) {
         "'" +
           err.field +
           "'" +
-          ' không đúng với mong đợi. Xem lại trường ảnh hoặc video gửi lên trong yêu cầu cho đúng'
-      );
+          ' không đúng với mong đợi. Xem lại trường ảnh hoặc video gửi lên trong yêu cầu cho đúng',
+      )
     }
   }
-  console.log(err);
-  return callRes(res, responseError.UNKNOWN_ERROR, 'Lỗi chưa xác định');
-});
+  console.log(err)
+  return callRes(res, responseError.UNKNOWN_ERROR, 'Lỗi chưa xác định')
+})
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server is running on port ${port}`))
